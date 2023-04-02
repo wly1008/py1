9675ccd09c9cc695f115b44d41ec0a22f5ec72b5
Liyei Wei <116957192+wly1008@users.noreply.github.com>
2023-04-02 21:19:05 +0800

    Update 8 dly处理.py

--- 8 dly处理.py
+++ 8 dly处理.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar 31 19:57:36 2023
+Created on Sun Apr  2 21:04:03 2023
 
 @author: wly
 """
@@ -8,16 +8,8 @@ Created on Fri Mar 31 19:57:36 2023
 import pandas as pd
 import os
 import re
-
-#创建文件夹
-def mkdir(path):
- 
-	folder = os.path.exists(path)
- 
-	if not folder:                   #判断是否存在文件夹如果不存在则创建为文件夹
-		os.makedirs(path)            #makedirs 创建文件时如果路径不存在会创建这个路径
-	else:
-		pass
+import calendar
+import numpy as np
 
 def read_data(file_paths):
     dfs = []
@@ -37,34 +29,45 @@ def process_data(x):
     df[['site', 'year', 'month', 'variable', 'data']] = x[0].str.extract(r'(\S{11})(\d{4})(\d{2})(\S{4})(.*)') # <<<<<正则表达式
     
     df[[i for i in range(1,32)]] = df['data'].apply(get_num)  # <<<<<<<<<<取出数字
-    
-    # 获得月天数
-    df['date'] = pd.to_datetime(df['year'] + '-' + df['month']) 
-    df['day'] = df['date'].dt.daysinmonth
-    return df.drop(['date','data'], axis=1)
+    df.replace('-9999',np.nan,inplace = True)
+    return df.drop(['data'], axis=1)
 
 # 分组各站点数据合并
 def func_site(x):
+    day_iter = iter(range(1,366+1))# <<<<<<<<<<<年天数迭代器
+    year = int(x.year.iat[0])# <<<<<<<<<<<年
     df = pd.DataFrame()
+    
+    # 按站点分组
     gros = x.groupby('site')
     for Site,gro_data in gros:
 
         # 按月天数合并年数据
-        df_all = pd.Series()
-        for i in range(len(gro_data)):
+        day_iter = iter(range(1,366+1))
+        dfn_all = pd.DataFrame()
+        for i in range(1,13):
             
-            ser = gro_data.iloc[i]
-            day = ser.day
-            dfn = ser.loc[1:day]
-            df_all = pd.concat([df_all,dfn])
-        # 重设索引
-        cols = iter(range(1,len(df_all)+1))
-        df_all = df_all.rename(index=lambda x: next(cols))
+            df_mon = gro_data[gro_data.month == '%.2d'%i]
+            day = calendar.monthrange(year,i)[1]# <<<<<<<<<<<<<<<<<获得月天数
+            dfn = df_mon.loc[:,1:day]
+            for n in range(day):
+                
+                if n == 0:
+                    mon_day = []
+                mon_day.append(next(day_iter))# 该年第几天
+            dfn.columns = mon_day
+            try:
+                dfn.index = [0]# <<<<<<<<<<<<<<<保证索引一致
+            except:
+                pass
+            dfn_all = pd.concat([dfn_all,dfn],axis=1)
+
         # 加上基础属性
-        df_all = pd.concat([ser[['site', 'variable', 'year']],df_all])
+        # dfn_all = pd.concat([df_mon[['site', 'variable', 'year']],dfn_all])
+        dfn_all['site'] = Site
         # 合并各站点数据
-        df = pd.concat([df,df_all])
-    return df
+        df = pd.concat([df,dfn_all])
+    return pd.concat([df.iloc[:,-1],df.iloc[:,:-1]],axis=1)
 
 if __name__ == '__main__':
     # 输入、输出
@@ -78,13 +81,20 @@ if __name__ == '__main__':
     gros = df_day.groupby(['year','variable'])
     
     for (Year,Var),gro_data in gros:
-
-        df_out = func_site(gro_data).T
+        df_out = func_site(gro_data)
         
         # 输出
         file = os.path.join(output_dir, Var)
-        mkdir(file)
+        if not (os.path.exists(file)):                   
+            os.makedirs(file)
         doc = f'{Var}_{Year}.txt'
         out_path = os.path.join(file, doc)
-        
-        # df_out.to_csv(out_path,sep='\t',index=0)
+        df_out.fillna('nan',inplace=True)
+        df_out.to_csv(out_path,sep='\t',index=0,header=0)
+        print(f'{Var}_{Year}')
+
+
+
+
+
+	
