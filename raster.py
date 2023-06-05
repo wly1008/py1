e9194ea2e2d9696c8e680a537b96ce2e5c378f1c
Liyei Wei <116957192+wly1008@users.noreply.github.com>
2023-06-05 21:33:22 +0800

    Update raster.py

--- raster.py
+++ raster.py
@@ -1,23 +1,39 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed May  3 10:32:33 2023
+Created on Fri Jun  2 14:59:27 2023
 
 @author: wly
 """
 
+
+
+from functools import partial
 import rasterio
 import pandas as pd
 import numpy as np
 from rasterio.enums import Resampling
 
 
-def read(path_tif,n = 1,tran = True, nan = np.nan, resampling = ('shape_', 'how'), print_ = False):
+
+
+def get(src,attrs):
+    
+    name = locals()
+    
+    returns = []
+    for attr in attrs:
+        name[attr] = getattr(src,attr)
+        returns.append(name[attr])
+    if len(returns) == 1:
+        returns = returns[0]
+    
+    return returns
+    
+
+def read(path_tif, n=1, tran=True, nan=np.nan, dtype=np.float64, 
+         re_shape=False, re_scale=False, re_len=False, how='nearest',printf=False):
     '''
-    -----------
-    
-    重采样未解决仿射变换问题，暂时无法直接使用，只能得到重采样后的矩阵，需配合正确仿射数据使用
-    
-    ----------
+
     path : string
         地址.
     n : 1 or 2 or 3, optional.
@@ -26,140 +42,200 @@ def read(path_tif,n = 1,tran = True, nan = np.nan, resampling = ('shape_', 'how'
         是否变为单列. The default is True.
     nan : optional
         无效值设置.The default is np.nan.
-    resampling : tuple,optional.
-        重采样.默认不开启，如需使用shape_、how都是必填的.
-        (shape_，how)
-        ---shape_---(tuple)
-        (src.count, src.height, src.width)
-        ---how---(str or int)
-        (部分)
-        mode:众数，6;
-        nearest:临近值，0;
-        bilinear:双线性，1;
-        cubic_spline:三次卷积，3。
-    print_ : 任意值,optional.
+    dtype : optional
+        矩阵值的格式. The default is np.float64.
+    
+    ---------------------------------------------------------------------------
+    单波段栅格
+    
+    重采样参数（re_shape=False, re_scale=False, re_len=False, how='nearest',printf=False）
+    
+    
+    
+    re_shape:形状重采样
+    (count, height, width)
+    
+    re_len:大小重采样
+    (xsize,ysize)
+    
+    re_scale:倍数重采样
+    scale = 目标边长大小/原数据边长大小
+    
+    ---how---(str or int)
+    默认'nearest'
+    
+    (部分)
+    mode:众数，6;
+    nearest:临近值，0;
+    bilinear:双线性，1;
+    cubic_spline:三次卷积，3。
+    
+    printf : 任意值,optional.
         如果发生重采样，则会打印原形状及输入值。The default is False.
+    
+    --------------------------------------------------------------------------- 
+    
+    
     Returns:
-        栅格矩阵（单列or原型）；meat;shape
+        栅格矩阵（单列or原型）；meta;shape
 
     '''
+
+    def updata():  #<<<<<<<<<更新函数
     
-    with rasterio.open(path_tif) as src:
-        nodata = np.float64(src.nodata)
-        # profile = src.profile
-        # profile.data['dtype'] = 'float64'
-        meat = src.meta
-        meat['dtype'] = 'float64'
-        shape = (src.count, src.height, src.width)
-        
-        #重采样
-        if resampling[0] != 'shape_':
+        if shape != shape_aim:
             
-            shape_ = resampling[0]
+            if printf:
+                print(shape, printf)
             
-            if shape != shape_:
-                
-                if not print_ is False:
-                    print(shape,print_)
-                    
-                shape = shape_
-                # profile.data.update({'height':shape_[1],'width':shape_[2]})
-                
-                meat.update({'height':shape_[1],'width':shape_[2]})
-                
-                # 重采样方法---how
-                
-                if type(resampling[1]) is int:
-                    how = resampling[1]
-                else:
-                    how = getattr(Resampling,resampling[1])
+            bounds = {'west': west, 'south': south, 'east': east,
+                      'north': north, 'height': shape_aim[1], 'width': shape_aim[2]}
+
+            transform = rasterio.transform.from_bounds(**bounds)
+
+            profile.data.update({'height': shape_aim[1], 'width': shape_aim[2], 'transform': transform})
             
-                data = src.read(out_shape=shape_,resampling=how).astype('float64')
+            if type(how) is int:
+                resampling = how
             else:
-                resampling = ('shape_',)
+                resampling = getattr(Resampling, how)
             
-        if resampling[0] == 'shape_':
-            data = src.read().astype('float64')
-        
-        # 变形
-        if tran:
-            data = data.reshape(-1,1)
+            
+            data = src.read(out_shape=shape_aim, resampling=resampling).astype(dtype)
         else:
-            data = data[0]
-        data = pd.DataFrame(data)
+            data = src.read().astype(dtype)
+            pass    
+        return data
+    
+    
+    src = rasterio.open(path_tif)
+    
+    # 取出所需参数
+    nodata, profile, count, height, width,transform = get(src,('nodata', 'profile', 'count', 'height', 'width','transform'))
+    
+    west, south, east, north = rasterio.transform.array_bounds(height, width, transform)
+    shape = (count, height, width)
+    nodata = dtype(nodata)
+    
+    
+    if re_shape:
+        shape_aim = re_shape
         
-        # 无效值处理
-        data.replace(nodata,nan,inplace=True)
-        # profile.data['nodata'] = nan
-        meat['nodata'] = nan
-    del src
+        # 更新矩阵、profile、shape
+        data = updata()
+        shape = shape_aim
     
     
+    elif re_len:
+        xsize,ysize = re_len
+        shape_aim = (count,int((north-south)/ysize),int((east-west)/xsize))
+        
+        # 更新
+        data = updata()
+        shape = shape_aim
+    
+    
+    
+    elif re_scale:
+        scale = re_scale
+        shape_aim = (count, int(height/scale), int(width/scale))
+        
+        # 更新
+        data = updata()
+        shape = shape_aim
+    
+
+    else:
+        data = src.read().astype(dtype)
+    
+    
+    if tran:
+        data = data.reshape(-1, 1)
+    else:
+        data = data[0]
+    data = pd.DataFrame(data)
+    data.replace(nodata, nan, inplace=True)
+    
+    src.close()
     # 返回
     if n == 1:
         return data
     elif n == 2:
-        # return data,profile
-        return data,meat
+        return data,profile
     elif n == 3:
-        # return data,profile,shape
-        return data,meat,shape
+        return data,profile,shape
     else:
         print('n=1 or 2 or 3')
- 
 
 
-
-
-def out(tif_path,df,shape,meat):
+def out(tif_path, df, shape, pro):
     '''
     操作函数
     ---------
-    
+
     输出文件函数
 
     '''
-    df = pd.DataFrame(np.array(df).reshape(shape)[0])  
-    
-    with rasterio.open(tif_path, 'w', **meat) as src:
-        src.write(df, 1) 
+    df = pd.DataFrame(np.array(df).reshape(shape)[0])
 
-
-
-
-def get_shape(path):
-    with rasterio.open(path) as src:
-        shape = src.shape
-    return shape
-
+    with rasterio.open(tif_path, 'w', **pro) as src:
+        src.write(df, 1)
 
 
 def mask(path_tif, path_mask, tif_path):
     '''
     操作函数，会直接输出
     ---------------------
-    
-    需保证meat一致才能正常使用
-    
+
+    需保证meta一致才能正常使用
+
     -----------------------------------
-    
+
     栅格提取栅格，在mask中有效值的位置，tif的相应位置值会被保留
 
 
     '''
-    
-    
-    df_tif, meat, shape = read(path_tif,3)
-    df_mask, meat_m, shape_m = read(path_mask,3)
 
-    if meat != meat_m:
-        print('meat不同')
-        return 
+    df_tif, pro, shape = read(path_tif, 3)
+    df_mask, pro_m, shape_m = read(path_mask, 3)
+
+    if pro != pro_m:
+        print('meta不同')
+        return
     mask = ~df_mask.isna()
 
     df = df_tif[mask]
 
-    out(tif_path, df, shape, meat)
+    out(tif_path, df, shape, pro)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
