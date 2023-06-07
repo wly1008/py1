72fca599f98191d586b3f89292e2ed886dcbb0ea
Liyei Wei <116957192+wly1008@users.noreply.github.com>
2023-06-07 11:30:24 +0800

    Update raster.py

--- raster.py
+++ raster.py
@@ -16,7 +16,7 @@ from rasterio.enums import Resampling
 
 
 
-def get(src,attrs):
+def get(src,*attrs):
     
     name = locals()
     
@@ -30,8 +30,18 @@ def get(src,attrs):
     return returns
     
 
-def read(path_tif, n=1, tran=True, nan=np.nan, dtype=np.float64, 
-         re_shape=False, re_scale=False, re_size=False, how='nearest',printf=False):
+
+
+
+def get_RasterArrt(path_in,*attrs):
+    
+    with rasterio.open(path_in) as src:
+        return get(src,*attrs)
+
+
+
+def read(path_in, n=1, tran=True, nan=np.nan, dtype=np.float64, 
+         re_shape=False, re_scale=False, re_size=False, how='nearest', printf=False):
     '''
 
     path : string
@@ -46,9 +56,9 @@ def read(path_tif, n=1, tran=True, nan=np.nan, dtype=np.float64,
         矩阵值的格式. The default is np.float64.
     
     ---------------------------------------------------------------------------
-    单波段栅格
     
-    重采样参数(re_shape=False, re_scale=False, re_len=False, how='nearest',printf=False)
+    
+    重采样参数（re_shape=False, re_scale=False, re_size=False, how='nearest',printf=False）
     
     
     
@@ -61,7 +71,7 @@ def read(path_tif, n=1, tran=True, nan=np.nan, dtype=np.float64,
     re_scale:倍数重采样
     scale = 目标边长大小/原数据边长大小
     
-    how:重采样方法(str or int)
+    ---how---(str or int)
     默认'nearest'
     
     (部分)
@@ -81,81 +91,91 @@ def read(path_tif, n=1, tran=True, nan=np.nan, dtype=np.float64,
 
     '''
 
-    def updata():  #<<<<<<<<<更新函数
+    def update():  #<<<<<<<<<更新函数
     
-        if shape != out_shape:
+        if shape != shape_aim:
             
             if printf:
                 print(shape, printf)
             
             bounds = {'west': west, 'south': south, 'east': east,
-                      'north': north, 'height': out_shape[1], 'width': out_shape[2]}
+                      'north': north, 'height': shape_aim[1], 'width': shape_aim[2]}
 
             transform = rasterio.transform.from_bounds(**bounds)
 
-            profile.data.update({'height': out_shape[1], 'width': out_shape[2], 'transform': transform})
+            profile.data.update({'height': shape_aim[1], 'width': shape_aim[2], 'transform': transform})
             
             if type(how) is int:
-                resampling = how
+                _resampling = how
             else:
-                resampling = getattr(Resampling, how)
+                _resampling = getattr(Resampling, how)
             
             
-            data = src.read(out_shape=out_shape, resampling=resampling).astype(dtype)
+            data = src.read(out_shape=shape_aim, resampling=_resampling).astype(dtype)
         else:
             data = src.read().astype(dtype)
             pass    
         return data
     
     
-    src = rasterio.open(path_tif)
+    src = rasterio.open(path_in)
     
     # 取出所需参数
-    nodata, profile, count, height, width,transform = get(src,('nodata', 'profile', 'count', 'height', 'width','transform'))
+    nodata, profile, count, height, width,transform = get(src,*('nodata', 'profile', 'count', 'height', 'width','transform'))
     
     west, south, east, north = rasterio.transform.array_bounds(height, width, transform)
     shape = (count, height, width)
     nodata = dtype(nodata)
     
-    # 重采样
+    
     if re_shape:
-        out_shape = re_shape
+        shape_aim = re_shape
         
         # 更新矩阵、profile、shape
-        data = updata()
-        shape = out_shape
+        data = update()
+        shape = shape_aim
     
     
     elif re_size:
         xsize,ysize = re_size
-        out_shape = (count,int((north-south)/ysize),int((east-west)/xsize))
+        shape_aim = (count,int((north-south)/ysize),int((east-west)/xsize))
         
         # 更新
-        data = updata()
-        shape = out_shape
+        data = update()
+        shape = shape_aim
+    
     
     
     elif re_scale:
         scale = re_scale
-        out_shape = (count, int(height/scale), int(width/scale))
+        shape_aim = (count, int(height/scale), int(width/scale))
         
         # 更新
-        data = updata()
-        shape = out_shape
+        data = update()
+        shape = shape_aim
     
 
     else:
-        data = src.read().astype(dtype)  #<<<<<<矩阵
+        data = src.read().astype(dtype)
     
-    # 变形
-    if tran:
-        data = data.reshape(-1, 1)
-    else:
-        data = data[0]
+    
+    # 处理无效值
+    data = data.reshape(-1, 1)
     data = pd.DataFrame(data)
     data.replace(nodata, nan, inplace=True)
     
+    # 变形
+    if tran:
+        pass
+    else:
+        data = np.array(data).reshape(shape)
+        if shape[0] == 1:
+            data = pd.DataFrame(data[0])
+    
     src.close()
+    
+    
+    
     # 返回
     if n == 1:
         return data
@@ -167,7 +187,7 @@ def read(path_tif, n=1, tran=True, nan=np.nan, dtype=np.float64,
         print('n=1 or 2 or 3')
 
 
-def out(tif_path, df, shape, pro):
+def out(out_path, data, shape, pro):
     '''
     操作函数
     ---------
@@ -175,13 +195,13 @@ def out(tif_path, df, shape, pro):
     输出文件函数
 
     '''
-    df = pd.DataFrame(np.array(df).reshape(shape)[0])
+    data = np.array(data).reshape(shape)
 
-    with rasterio.open(tif_path, 'w', **pro) as src:
-        src.write(df, 1)
+    with rasterio.open(out_path, 'w', **pro) as src:
+        src.write(data)
 
 
-def mask(path_tif, path_mask, tif_path):
+def mask(path_in, path_mask, out_path):
     '''
     操作函数，会直接输出
     ---------------------
@@ -190,34 +210,50 @@ def mask(path_tif, path_mask, tif_path):
 
     -----------------------------------
 
-    栅格提取栅格，在mask中有效值的位置，tif的相应位置值会被保留
+    栅格提取栅格，在掩膜（mask）中有效值的位置，输入栅格（in）的相应位置值会被保留
 
 
     '''
 
-    df_tif, pro, shape = read(path_tif, 3)
+    df_tif, pro, shape = read(path_in, 3)
     df_mask, pro_m, shape_m = read(path_mask, 3)
-
-    if pro != pro_m:
-        print('meta不同')
+    
+    
+    meta = get_RasterArrt(path_in,'meta')
+    meta_m = get_RasterArrt(path_mask,'meta')
+    
+    
+    
+    if meta != meta_m:
+        print('meat不同')
         return
     mask = ~df_mask.isna()
 
     df = df_tif[mask]
 
-    out(tif_path, df, shape, pro)
+    out(out_path, df, shape, pro)
 
 
 
 
 
+def resampling(path_in, out_path, nan=np.nan, dtype=np.float64, 
+               re_shape=False, re_scale=False, re_size=False, how='nearest', printf=False):
+    
+    
+    '''
+    操作函数，直接输出
+    
+    '''
 
-
-
-
-
-
-
+    data, pro, shape = read(path_in, nan=nan, dtype=dtype,
+                            re_size=re_size, re_scale=re_scale, re_shape=re_shape,
+                            how=how, printf=printf)
+    
+    
+    out(out_path, data, shape, pro)
+    
+    
 
 
 
